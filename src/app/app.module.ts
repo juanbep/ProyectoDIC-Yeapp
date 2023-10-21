795f15a6984f2a6b141b75b8db5fe7dad8131d1b
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2023-10-21 16:19:05 -0500

    new6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,17 +15,29 @@ import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProgressComponent } from './componets/progress/progress.component';
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     Facturav1Component,
-    ValuePayComponent
+    ValuePayComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards}), BrowserAnimationsModule,
-    MatToolbarModule,MatCardModule, MatButtonModule, MatListModule, MatDividerModule, MatGridListModule,MatExpansionModule
+    MatToolbarModule,MatCardModule, MatButtonModule, MatListModule, MatDividerModule, 
+    MatGridListModule,MatExpansionModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
