7e70abbabc47b11c8d73b564d0c78084770728ef
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2023-10-21 13:18:57 -0500

    ValuePay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './componets/side-bar/side-bar.component';
 //icons
 import { LucideAngularModule, File, Home, Menu, UserCheck,BarChart3,WalletCards } from 'lucide-angular';
-
+import { ValuePayComponent } from './componets/value-pay/value-pay.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    ValuePayComponent
   ],
   imports: [
-    BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards})
+    BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards}), BrowserAnimationsModule,MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
