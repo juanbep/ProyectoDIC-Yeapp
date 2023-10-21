6d084b10dff0222c19e2a39f6959288206a8420f
juanbep <jbecapillimue@gmail.com>
2023-10-21 14:10:07 -0500

    commit beca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,27 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './componets/side-bar/side-bar.component';
 //icons
 import { LucideAngularModule, File, Home, Menu, UserCheck,BarChart3,WalletCards } from 'lucide-angular';
+import { Facturav1Component } from './componets/factura/facturav1/facturav1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    Facturav1Component
   ],
   imports: [
-    BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards})
+    BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards}), BrowserAnimationsModule,
+    MatToolbarModule,MatCardModule, MatButtonModule, MatListModule, MatDividerModule, MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componets/factura/facturav1/facturav1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-facturav1',
+  templateUrl: './facturav1.component.html',
+  styleUrls: ['./facturav1.component.css']
+})
+export class Facturav1Component {
+
+}
