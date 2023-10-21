681452c2d8354cc146a720290384b713cbc0f51a
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2023-10-21 11:05:54 -0500

    sideBar-v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './componets/side-bar/side-bar.component';
+//icons
+import { LucideAngularModule, File, Home, Menu, UserCheck,BarChart3,WalletCards } from 'lucide-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,LucideAngularModule.pick({File, Home, Menu, UserCheck,BarChart3,WalletCards})
   ],
   providers: [],
   bootstrap: [AppComponent]
