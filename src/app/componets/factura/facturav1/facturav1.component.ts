bbe83b0bc549dd732b34036f534f224d2c64bf19
juanbep <jbecapillimue@gmail.com>
2023-10-21 20:49:49 -0500

    ultimo commit

--- src/app/componets/factura/facturav1/facturav1.component.ts
+++ src/app/componets/factura/facturav1/facturav1.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-facturav1',
   templateUrl: './facturav1.component.html',
   styleUrls: ['./facturav1.component.css']
 })
 export class Facturav1Component {
-
+  tiles: Tile[] = [
+    {text: 'edificio1.jpg', cols: 1, rows: 2},
+    {text: 'edificio2.jpg', cols: 1, rows: 2},
+    {text: 'edificio4.jpg', cols: 1, rows: 2},
+    {text: 'edificio5.jpg', cols: 3, rows: 2},
+  ];
 }
